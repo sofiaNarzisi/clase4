e8b20916d1170fc186ebf74169413d39a2617966
sofiaNarzisi <135072779+sofiaNarzisi@users.noreply.github.com>
2023-05-30 19:12:55 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  const apodo = "Sofy"
+  const expectativa = "Anime"
+  return (
+    <>
+  <h1>¡Hola soy {apodo}!</h1>
+  <p>Quisiera contarte que me gustaría hacer una página o aplicación sobre {expectativa}</p>
+  <p>Contanos que más te gustaría hacer con react? ¿Cuales son tus metas como futuro Devs Front-end?</p>
+  <ul>
+    <li>Una aplicacion en la cual puedas indagar sobre los diversos animes</li>
+    <li>llegar a transmitirlos sin ninguna obstrucción</li>
+    
+  </ul>
+    </>
+  )
+}
+
+export default App
